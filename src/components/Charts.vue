e291b235321200c4f032e23be01cc225d0f49582
Luis yChun <yongchun903@gmail.com>
2020-03-31 22:06:33 +0800

    add Charts component

---
+++ src/components/Charts.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Charts</p>
+    <line-chart :data="lists" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Charts",
+  data() {
+    return {
+      lists: [
+        { name: "Workout", data: { "01-01-2017": 3, "2017-01-02": 4 } },
+        { name: "Call parents", data: { "2017-01-01": 5, "2017-01-02": 3 } }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
