e69ad29480125ca37b9ae49b68fdab86f1f55f90
Luis yChun <yongchun903@gmail.com>
2020-04-10 18:07:36 +0800

    add dropdown select list to filter chart by each category cases

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <line-chart :data="fetchData()" :min="1"></line-chart>
+    <v-row class="justify-center text-center">
+      <v-col cols="3">
+        <v-select
+          :items="items"
+          item-value="items"
+          label="Filter Case"
+          outlined
+          @change="changeFilter"
+        ></v-select>
+      </v-col>
+    </v-row>
+    <line-chart :data="fetchData(caseFilter)" :min="1"></line-chart>
   </div>
 </template>
 
@@ -9,21 +20,42 @@ export default {
   name: "Charts",
   props: ["confirm", "death", "recover"],
   data() {
-    return {};
+    return {
+      caseFilter: "All",
+      items: ["All", "Confirmed", "Death", "Recovered"]
+    };
   },
   methods: {
-    fetchData() {
+    changeFilter(ans) {
+      this.caseFilter = ans;
+      this.fetchData(this.caseFilter);
+    },
+    fetchData(filter) {
       let list = new Array();
+      let confirmList = [],
+        deathList = [],
+        recoverList = [];
       list = [
         { name: "Confirm", data: this.confirm },
         { name: "Death", data: this.death },
         { name: "Recover", data: this.recover }
       ];
-      return list;
+      confirmList = [{ name: "Confirm", data: this.confirm }];
+      deathList = [{ name: "Death", data: this.death }];
+      recoverList = [{ name: "Recover", data: this.recover }];
+      if (filter == "All") {
+        return list;
+      } else if (filter == "Confirmed") {
+        return confirmList;
+      } else if (filter == "Death") {
+        return deathList;
+      } else {
+        return recoverList;
+      }
     }
   },
   created() {
-    this.fetchData();
+    this.fetchData(this.caseFilter);
   }
 };
 </script>
